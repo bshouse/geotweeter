6e603cb1e00663022461225cf15f2f665e6752d0
William DeRaad <william.deraad@colorado.edu>
2015-04-11 12:42:42 -0600

    Sinatra app updated to server identical content to express, however not rendering the same.

--- geoapp.rb
+++ geoapp.rb
@@ -7,7 +7,36 @@ require 'json/ext'
 
 include Mongo
 
+ mongo_client = MongoClient.new("localhost", 27017)
+ db = mongo_client.db('csci')
+ coll = db.collection('summary')
 
+
+#Location of static pages
+set :public_folder, File.dirname(__FILE__) + '/static'
+#set :port, 3000
+
+#Default Page
 get '/' do 
-	'Hello from geotweeter!'
+	send_file File.join(settings.public_folder, 'hotspots.html')
 end
+
+#Summary content from Database
+get '/summary' do
+	content_type :json
+	if params[:location]
+		loc = params[:location]
+	else 
+		loc = "World"
+	end
+	puts loc
+	coll.find("name" => loc).to_a.to_json
+end
+
+#Static Pages
+get '/:name' do
+	send_file File.join(settings.public_folder, params[:name])
+end
+
+
+
