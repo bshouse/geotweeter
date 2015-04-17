862b3bebb8eaf38c0abbe80b0ffbe6cf7ce8d371
Brett Shouse <idahoincolorado@gmail.com>
2015-04-17 11:33:19 -0600

    Add HotSpot tab and prepare for pre-compile JSX

---
+++ static/app.js
@@ -0,0 +1,33 @@
+/** @jsx React.DOM */
+'use strict';
+
+var Tabs = ReactSimpleTabs;
+var App = React.createClass({
+  onMount: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
+    console.log('on mount, showing tab ' + selectedIndex);
+  },
+  onBeforeChange: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
+    console.log('before the tab ' + selectedIndex);
+  },
+  onAfterChange: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
+    console.log('after the tab ' + selectedIndex);
+  },
+  render: function() {
+    return (
+      <Tabs tabActive={1} onBeforeChange={this.onBeforeChange} onAfterChange={this.onAfterChange} onMount={this.onMount}>
+        <Tabs.Panel title='Hot Spots'>
+        <div id='world'></div>
+        <div id='country'></div>
+        </Tabs.Panel>
+        <Tabs.Panel title='Country Map'>
+          <h2>Country Map Goes here</h2>
+        </Tabs.Panel>
+        <Tabs.Panel title='Twitter Tables'>
+          <h2>Twitter Tables Go here</h2>
+        </Tabs.Panel>
+      </Tabs>
+    );
+  }
+});
+
+React.renderComponent(<App />, document.getElementById('tabs'));
