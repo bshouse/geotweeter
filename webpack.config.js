6870dddfe5507227d890a71428a12b8e75bf352a
Brett Shouse <idahoincolorado@gmail.com>
2015-04-19 14:39:26 -0600

    removed file renaming to eliminate webpack confusion

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ var _ = require('lodash');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var LIB_NAME = 'react-simpletabs';
-var DIST_NAME = 'react-simpletabs-dist';
 
 
 var banner = _.template([
@@ -15,7 +14,7 @@ var banner = _.template([
   ' @license <%= pkg.license %>',
   ' @author <%= pkg.author.name %> (<%= pkg.author.url %>)',
   ''
-  
+
 ].join('\n'), {
   pkg: require('./package.json')
 });
@@ -25,7 +24,7 @@ module.exports = {
   entry: './lib/' + LIB_NAME + '.jsx',
   output: {
     path: __dirname + '/static',
-    filename: DIST_NAME + '.js',
+    filename: LIB_NAME + '.js',
     libraryTarget: 'umd',
     library: 'ReactSimpleTabs'
   },
