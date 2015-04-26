afea0e3450bb21d550aacf778f1a3f287316c2b9
Brett Shouse <idahoincolorado@gmail.com>
2015-04-26 13:59:00 -0600

    Add a travel summary

--- static/app.js
+++ static/app.js
@@ -25,6 +25,9 @@ var App = React.createClass({
       case 4:
         users.showIt();
         break;
+      case 5:
+        travel.showIt();
+        break;
       default:
         console.log('unknown tab: '+selectedIndex)
         break;
@@ -54,6 +57,12 @@ var App = React.createClass({
           <div id="smallCountsChart"></div>
           <h2>Screen Names Changes</h2>
           <div id="screenNameChart"></div>
+        </Tabs.Panel>
+        <Tabs.Panel title='Travel'>
+          <h2>Number of users by Country count.</h2>
+          <div id="countryTravelChart"></div>
+          <h2>Number of users by State/Province count.</h2>
+          <div id="stateTravelChart"></div>
         </Tabs.Panel>
       </Tabs>
     );
